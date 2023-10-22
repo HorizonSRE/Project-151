2f9f07f750e3c5cae38aadbc642b0f63890c3878
HorizonSRE <106289299+HorizonSRE@users.noreply.github.com>
2023-10-22 16:50:27 -0700

    Add files via upload

---
+++ Class-151.py
@@ -0,0 +1,10 @@
+month = ("January", "February", "March", "April","May",  "June", "July", "August", "September", "October", "November", "December")
+profits = (20000, 45000, 78000, 97000, 12000, 456000, 65000, 54000, 10000, 30000, 70000, 90000)
+max_profits=max(profits)
+max_profits_index=profits.index(max_profits)
+max_profit_month=month[max_profits_index]
+print("The maximum profit of " + str(max_profits) + " was recorded on the month of "+ str(max_profit_month))
+min_profits=min(profits)
+min_profits_index=profits.index(min_profits)
+min_profit_month=month[min_profits_index]
+print("The minimum profit of " + str(min_profits) + " was recorded on the month of "+ str(min_profit_month))
